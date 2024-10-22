a7430a4a9887cc24a9341555875d8c578c29b7b1
tarek ammari <tarekammari1@gmail.com>
2024-10-22 22:15:08 +0100

    Horizental Scroll Testimonial

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Nav  from './Pages/Home_Page/Nav_Section/Nav';
 import Main from './Pages/Home_Page/Main_Section/Main';
 import Menu_Section from './Pages/Home_Page/Menu_Section/Menu_Section';
 import Testimonials from './Pages/Home_Page/Testimonials_Section/Testimonials_Section';
+import About from './Pages/Home_Page/About_Section/About_Section';
 import "./App.css"
 import { header } from "react"
 
@@ -15,6 +16,7 @@ function App() {
           <Main />
           <Menu_Section />
           <Testimonials />
+          <About />
       </header>
       
     </div>
---
+++ src/Pages/Home_Page/About_Section/About_Section.js
@@ -0,0 +1,9 @@
+import "./About_Section.css";
+
+export default function About_Section () {
+    return (
+      <div className="About_Section">
+        <h2 className="Section_title">About</h2>        
+      </div>
+    );
+  };
